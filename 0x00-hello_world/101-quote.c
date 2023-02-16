fd7edcdbe4e5efe89e4392040cfd08d2e90d36f3
Moses <nyagudi.ulridge@strathmore.edu>
2023-02-16 06:35:47 -0800

    Create 0x00-hello_world folder

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <studio.h>
+/**
+ * Function that returns 1 as its output
+ */
+int main(void)
+{
+	int number = 0;
+	char quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	while (quote[number] != '\0')
+	{
+		putchar(quote[number]);
+		number++;
+	}
+	return(1);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,2 @@
+## Introduction to programming
+This repository contains basic variables for C programming language
