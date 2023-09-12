835301e076ed3000d10b5d32329f74301a34d994
Moses-Nyagudi <nyagudi.ulridge@strathmore.edu>
2023-09-12 03:11:45 -0700

    Corrections

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,32 +1,21 @@
 #include "main.h"
 
 /**
- * main - entry point of function.
- * Description: Print "_putchar" with custom header.
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
  * Return: 0
  */
 
-
-/**
- * displayString - prints array contents
- */
-
-void displayString(const char output[])
-{
-	int i = 0;
-
-	while (output[i] != '\0')
-	{
-		_putchar(output[i]);
-		i++;
-	}
-}
-
 int main(void)
 {
-	char output[] = "_putchar";
-	displayString(output);
+	char myVar[] = "_putchar";
+	int i = 0;
 
+	while (myVar[i] != '\0')
+	{
+		_putchar(myVar[i]);
+		i++;
+	}
 	_putchar('\n');
 
 	return (0);
