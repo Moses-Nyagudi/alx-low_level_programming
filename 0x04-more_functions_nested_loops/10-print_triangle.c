947592298897094506c9958df9421743dd32ed6e
Moses-Nyagudi <nyagudi.ulridge@strathmore.edu>
2023-09-14 05:09:18 -0700

    Triangle correction

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,40 +1,47 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a triangle followed by a new line
- * @size: The size of the triangle
+ * print_triangle - print a triangle at a given size.
+ * @size: size of triangle to print.
  */
-
 void print_triangle(int size)
 {
+	/**
+	 * int i: for iteration in for loop also used as a way to subtract.
+	 * int margin: will record the amount of whitespace I'll need to print.
+	 * int dis"tance": is used to offset size.
+	 * int to_print: is used to tell how many '#' will
+	 * need to be printed per-line.
+	 */
+
 	int i;
 
-	int j;
+	int margin;
 
-	int k;
+	int dis;
+
+	int to_print;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-
-	for (i = 0; i <= size ; i++)
+	dis = size - 1;
+	for (i = 0; i < size; i++)
 	{
-		for (j = size - 1 - i; j > 0; j--)
+		margin = 0;
+		while (margin < (dis - i))
 		{
-			_putchar(' ');
+			_putchar(32);
+			margin++;
 		}
-		
-		for (k = i + 1; k > 0; k--)
+		to_print = size - margin;
+		while (to_print > 0)
 		{
 			_putchar('#');
+			to_print--;
 		}
-
 		_putchar('\n');
 	}
 }
-
-
-
-
