33581a969884d77de6905bf10c4e97dd22e301e3
Moses-Nyagudi <99748377+Moses-Nyagudi@users.noreply.github.com>
2023-09-21 09:49:54 +0300

    corrections

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,19 +7,16 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n;
+  int n;
+  int a[5];
+  int *p;
 
-	int a[5];
-
-	int *p;
-
-	a[2] = 1024;
-	p = &n;
-	n = 98;
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+  a[2] = 1024;
+  p = &n;
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
-
