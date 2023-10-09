8a5cd7f14399d42e027f7f4ae8a4f2982fa0b6ad
Moses-Nyagudi <nyagudi.ulridge@strathmore.edu>
2023-10-09 00:48:15 -0700

    Strructures & Typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - Initializes a dog structure.
+ * @d: Pointer to the dog structure to initialize.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: void
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
